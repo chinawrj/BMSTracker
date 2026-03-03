b4b3575fe3fb2ce2a901a35ddcd3b624bdade588
Rujun Wang <chinawrj@gmail.com>
2026-03-03 20:40:39 +0800

    Initial Commit

---
+++ BMSTracker/BMSTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMSTrackerApp.swift
+//  BMSTracker
+//
+//  Created by 王如军 on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BMSTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMSTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMSTracker
+//
+//  Created by 王如军 on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
